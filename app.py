09e109d16dba12fc9eb5ca83f5b47dcac1fa920a
Luis Eduardo Garcia <garcia_luis81@hotmail.com>
2024-08-01 19:49:15 +0100

    actualizado requirements

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 import os
-#from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename
 import tensorflow as tf
 
 
